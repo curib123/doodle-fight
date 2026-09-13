d093db9d03ff77c570289bf16286562af9ea98f7
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-13 20:47:12 +0800

    extend player with aim health and respawn lifecycle

--- Classes/Player.h
+++ Classes/Player.h
@@ -9,12 +9,34 @@ public:
 
     void setMoveAxis(float axis);
     void setJetpackActive(bool active);
+    void setAimDirection(const cocos2d::Vec2& direction);
+
     void simulate(float dt, float groundY, float minX, float maxX);
+    void updateLifecycle(float dt, const cocos2d::Vec2& respawnPosition);
+
+    bool takeDamage(float amount);
+
+    const cocos2d::Vec2& aimDirection() const { return aimDirection_; }
+    const cocos2d::Vec2& velocity() const { return velocity_; }
+    float health() const { return health_; }
+    float maxHealth() const { return maxHealth_; }
+    bool isAlive() const { return alive_; }
+    float hitRadius() const { return hitRadius_; }
 
 private:
     cocos2d::Vec2 velocity_{0.0f, 0.0f};
+    cocos2d::Vec2 aimDirection_{1.0f, 0.0f};
+    cocos2d::Node* weaponPivot_{nullptr};
+
     float moveAxis_{0.0f};
     bool jetpackActive_{false};
+
+    float health_{100.0f};
+    float maxHealth_{100.0f};
+    bool alive_{true};
+    float respawnTimer_{0.0f};
+    float respawnDelay_{2.0f};
+    float hitRadius_{27.0f};
 
     float moveAcceleration_{1050.0f};
     float maxMoveSpeed_{260.0f};
